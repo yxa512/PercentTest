57ec196f960c5bdd868c3373aa2efe4e3a8db0ae
yxa512 <624511760@qq.com>
2018-04-27 15:15:36 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/percenttest/MainActivity.java
+++ app/src/main/java/com/example/administrator/percenttest/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         List<String> list = new ArrayList();
         list.add("123457896");
-        list.add("996");
+        list.add("995");
         list.add("456");
         list.add("789");
         list.add("123456");
